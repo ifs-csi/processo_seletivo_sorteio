359b0365fef2e91706391170ea640a497fdbb7ed
Carlson Santana Cruz <carlson.cruz@ifs.edu.br>
2020-12-07 15:04:33 -0300

    Sortear números dos candidatos através de uma lista de candidatos

--- sorteio.py
+++ sorteio.py
@@ -1,2 +1,6 @@
 def sortear_numero_candidato(candidato, sorteador):
     candidato['numero_sorteado'] = sorteador()
+
+def sortear_numeros_candidatos(candidatos, sorteador):
+    for candidato in candidatos:
+        sortear_numero_candidato(candidato, sorteador)
--- tests/sorteio.py
+++ tests/sorteio.py
@@ -5,13 +5,32 @@ import sorteio
 class TestSorteio(unittest.TestCase):
     def test_atribuir_numero_sorteado(self):
         def gerar_numero():
-            for i in range(1, 100):
+            for i in range(0, 100):
                 yield i
         generator = gerar_numero()
         def sorteador():
             return next(generator)
 
-        for i in range(1, 100):
+        for i in range(0, 100):
             candidato = {}
             sorteio.sortear_numero_candidato(candidato, sorteador)
+            self.assertEqual(i, candidato['numero_sorteado'])
+
+    def test_atribuir_numeros_sorteados_lista(self):
+        def gerar_numero():
+            for i in range(0, 100):
+                yield i
+        generator = gerar_numero()
+        def sorteador():
+            return next(generator)
+
+        candidatos = []
+        for i in range(0, 100):
+            candidato = {}
+            candidatos.append(candidato)
+
+        sorteio.sortear_numeros_candidatos(candidatos, sorteador)
+
+        for i in range(0, 100):
+            candidato = candidatos[i]
             self.assertEqual(i, candidato['numero_sorteado'])
