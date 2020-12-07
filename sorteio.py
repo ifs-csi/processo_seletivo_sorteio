41c548722ccf30470997b05af99ddb3fd89cb0a4
Carlson Santana Cruz <carlson.cruz@ifs.edu.br>
2020-12-07 15:54:50 -0300

    Verificar se um número já foi sorteado

--- sorteio.py
+++ sorteio.py
@@ -4,3 +4,14 @@ def sortear_numero_candidato(candidato, sorteador):
 def sortear_numeros_candidatos(candidatos, sorteador):
     for candidato in candidatos:
         sortear_numero_candidato(candidato, sorteador)
+
+def numero_ja_sorteado(candidato, candidatos):
+    for outro_candidato in candidatos:
+        if candidato is outro_candidato:
+            return False
+        if not ('numero_sorteado' in outro_candidato):
+            return False
+        if outro_candidato['numero_sorteado'] == candidato['numero_sorteado']:
+            return True
+
+    return False
--- tests/sorteio.py
+++ tests/sorteio.py
@@ -34,3 +34,17 @@ class TestSorteio(unittest.TestCase):
         for i in range(0, 100):
             candidato = candidatos[i]
             self.assertEqual(i, candidato['numero_sorteado'])
+
+    def test_numero_ja_sorteado(self):
+        candidatos = []
+        for i in range(0, 100):
+            candidato = {'numero_sorteado': i}
+            candidatos.append(candidato)
+
+        for i in range(0, 100):
+            candidato = {'numero_sorteado': i}
+            self.assertTrue(sorteio.numero_ja_sorteado(candidato, candidatos))
+
+        for i in range(100, 200):
+            candidato = {'numero_sorteado': i}
+            self.assertFalse(sorteio.numero_ja_sorteado(candidato, candidatos))
