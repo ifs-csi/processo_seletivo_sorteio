92d2249c953491c3934af09fc5abe27ea2dc4d5a
Carlson Santana Cruz <carlson.cruz@ifs.edu.br>
2020-12-22 15:33:30 -0300

    Criar uma classe para o sorteador de forma que não exista efeitos colaterais

--- sorteador_adapter.py
+++ sorteador_adapter.py
@@ -5,3 +5,10 @@ def definir_seed(nova_seed):
 
 def sorteador():
     return np.random.randint(1, ((2**32) - 1))
+
+class Sorteador:
+    def __init__(self, seed):
+        self.sorteador = np.random.RandomState(seed=seed)
+
+    def sortear(self):
+        return self.sorteador.randint(1, ((2**32) - 1))
--- test.py
+++ test.py
@@ -4,6 +4,7 @@ from tests.sorteio import TestSorteio
 from tests.agrupamento import TestAgrupamento
 from tests.input_csv import TestInputCSV
 from tests.output_csv import TestOutputCSV
+from tests.sorteador_adapter import TestSorteadorAdapter
 
 if __name__ == '__main__':
     unittest.main()
